189aa75b2d02734db295cb4ea8b68cd49a6fd011
Thorben Westerhuys <thorben@cividi.ch>
2021-12-07 14:09:54 +0100

    better layout

--- page/src/components/ParticipationLanding.vue
+++ page/src/components/ParticipationLanding.vue
@@ -1,6 +1,6 @@
 <template>
-  <div style="margin-top: -80px;">
-    <h1>Welcome to the dføur Xmas Mapping Challenge 2021!</h1>
+  <div style="margin-top: -180px;">
+    <h2>Welcome to the dføur Xmas Mapping Challenge 2021!</h2>
     <p>
       <a href="https://xmas.dfour.io/europe">
         <v-img
@@ -12,36 +12,42 @@
     </p>
     <p style="text-align:center;">
       <v-btn
-        color="primary"
-        large
+        color="#A13B3E"
+        dark large
         href="https://xmas.dfour.io/europe"
         >
         Start Mapping
       </v-btn>
     </p>
-    <h2>The Goal</h2>
+    <h3>The Goal</h3>
     <p>
       Map the temporary and create an overview of all Christmas Delights (food, drinks, decoration, events). Whether you are on your way as a tourist or a resident, stop for a second and tell the world about it.
     </p>
-    <h2>The Challenge</h2>
+    <h3>The Challenge</h3>
     <p>
-      The top contributors will be rewarded with a data towel of their city.
+      <ul>
+        <li>Is open for anyone: locals & tourist, all across the world.</li>
+        <li>No expertise required</li>
+        <li>Minimum data collection, not stored beyond the campaign</li>
+        <li>Cities with multiple entries: easier access via the left panel city list</li>
+        <li>The top contributors will be rewarded with a <a href="https://drive.google.com/file/d/1Hzep9f5vFaY65DqOXQcVIQnrTq1CJSA2/view?usp=sharing" target="_blank">data towel</a> of their city</li>
+      </ul>
     </p>
-    <h2>The Timing</h2>
+    <h3>The Timing</h3>
     <p>
       Inputs: 6-24. December 2021<br>
       Results: January 2022
     </p>
-    <h2>Sharing the Joy</h2>
+    <h3>Sharing the Joy</h3>
     <p>
-      Show your friends how you see Christmas!
+      Show with your friends how you experience the holiday season!
     </p>
-    <h2>The Team Behind</h2>
+    <h3>The Team Behind</h3>
     <p>
       The project is initiated by <a href="https://www.cividi.ch/en/" target="_blank">cividi</a>, and run on <a href="https://www.dfour.space/en/" target="_blank">dføur</a>, the collaborative mapping platform.
     </p>
     <p>
-      Contact: Viktoria Molnar, COO cividi GmbH, viktoria[et]cividi[dot]ch
+      Contact: Viktoria Molnar, COO cividi GmbH, viktoria [at] cividi [dot] ch
     </p>
     <v-row>
       <v-col
@@ -81,9 +87,17 @@ div {
 }
 a {
   text-decoration: none;
+  color: #A13B3E;
+  font-weight: bold;
   /* color: #fff;
   background-color: #543076;
   padding: 2px 4px; */
+}
+h2 {
+  padding-bottom: 20px;
+}
+h3 {
+  color: #1F5521;
 }
 p {
   font-family: Helvetica, Arial, sans-serif;
